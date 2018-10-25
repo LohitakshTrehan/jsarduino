ba73bdc0b29234bd9dcc385e175a94a196d31324
LohitakshTrehan <lohitakshtrehan@gmail.com>
2018-10-25 10:48:18 +0530

    Css for automatic
    Signed-off-by: LohitakshTrehan <lohitakshtrehan@gmail.com>

--- views/auto.js
+++ views/auto.js
@@ -5,7 +5,13 @@ $(window).on('load', function () {
     var led_3 = document.getElementById('led3')
     var led_4 = document.getElementById('led4')
     var led_5 = document.getElementById('led5')
-    $.get('/alloff')
+    $.get('/alloff',function(){
+        led_1.style.backgroundColor="RED"
+        led_2.style.backgroundColor="RED"
+        led_3.style.backgroundColor="RED"
+        led_4.style.backgroundColor="RED"
+        led_5.style.backgroundColor="RED"
+    })
     var id = null
     var working = false;
     var current_intensity=null;
@@ -17,6 +23,11 @@ $(window).on('load', function () {
                 id=null
             }
             led_1.innerHTML="OFF";led_2.innerHTML="OFF";led_3.innerHTML="OFF";led_4.innerHTML="OFF";led_5.innerHTML="OFF";
+            led_1.style.backgroundColor="RED"
+            led_2.style.backgroundColor="RED"
+            led_3.style.backgroundColor="RED"
+            led_4.style.backgroundColor="RED"
+            led_5.style.backgroundColor="RED"
             location.reload();
         })
     }
@@ -24,21 +35,51 @@ $(window).on('load', function () {
         $.get('/sensor',function(val){
             if(val==500){
                 led_1.innerHTML="OFF";led_2.innerHTML="OFF";led_3.innerHTML="OFF";led_4.innerHTML="OFF";led_5.innerHTML="OFF";
+                led_1.style.backgroundColor="RED"
+                led_2.style.backgroundColor="RED"
+                led_3.style.backgroundColor="RED"
+                led_4.style.backgroundColor="RED"
+                led_5.style.backgroundColor="RED"
               }
               else if(val>400){
                 led_1.innerHTML="ON";led_2.innerHTML="OFF";led_3.innerHTML="OFF";led_4.innerHTML="OFF";led_5.innerHTML="OFF";
+                led_1.style.backgroundColor="GREEN"
+                led_2.style.backgroundColor="RED"
+                led_3.style.backgroundColor="RED"
+                led_4.style.backgroundColor="RED"
+                led_5.style.backgroundColor="RED"
               }
               else if(val>300){
                 led_1.innerHTML="ON";led_2.innerHTML="ON";led_3.innerHTML="OFF";led_4.innerHTML="OFF";led_5.innerHTML="OFF";
+                led_1.style.backgroundColor="GREEN"
+                led_2.style.backgroundColor="GREEN"
+                led_3.style.backgroundColor="RED"
+                led_4.style.backgroundColor="RED"
+                led_5.style.backgroundColor="RED"
               }
               else if(val>200){
                 led_1.innerHTML="ON";led_2.innerHTML="ON";led_3.innerHTML="ON";led_4.innerHTML="OFF";led_5.innerHTML="OFF";
+                led_1.style.backgroundColor="GREEN"
+                led_2.style.backgroundColor="GREEN"
+                led_3.style.backgroundColor="GREEN"
+                led_4.style.backgroundColor="RED"
+                led_5.style.backgroundColor="RED"
               }
               else if(val>100){
                 led_1.innerHTML="ON";led_2.innerHTML="ON";led_3.innerHTML="ON";led_4.innerHTML="ON";led_5.innerHTML="OFF";
+                led_1.style.backgroundColor="GREEN"
+                led_2.style.backgroundColor="GREEN"
+                led_3.style.backgroundColor="GREEN"
+                led_4.style.backgroundColor="GREEN"
+                led_5.style.backgroundColor="RED"
               }
               else{
                 led_1.innerHTML="ON";led_2.innerHTML="ON";led_3.innerHTML="ON";led_4.innerHTML="ON";led_5.innerHTML="ON";
+                led_1.style.backgroundColor="GREEN"
+                led_2.style.backgroundColor="GREEN"
+                led_3.style.backgroundColor="GREEN"
+                led_4.style.backgroundColor="GREEN"
+                led_5.style.backgroundColor="GREEN"
               }
             id = setTimeout(fun,2000);
         })
@@ -56,6 +97,11 @@ $(window).on('load', function () {
             $.get('/alloff',function(){
                 working=!working;
                 led_1.innerHTML="OFF";led_2.innerHTML="OFF";led_3.innerHTML="OFF";led_4.innerHTML="OFF";led_5.innerHTML="OFF";
+                led_1.style.backgroundColor="RED"
+                led_2.style.backgroundColor="RED"
+                led_3.style.backgroundColor="RED"
+                led_4.style.backgroundColor="RED"
+                led_5.style.backgroundColor="RED"
             })
         }
     }

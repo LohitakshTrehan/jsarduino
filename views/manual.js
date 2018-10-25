cd1e64cec7452f37e6f7f6a8928826c077ce4bfe
LohitakshTrehan <lohitakshtrehan@gmail.com>
2018-10-25 10:39:35 +0530

    Css for manual
    Signed-off-by: LohitakshTrehan <lohitakshtrehan@gmail.com>

--- views/manual.js
+++ views/manual.js
@@ -1,6 +1,12 @@
 $(window).on('load', function () {
     console.log("window loaded");
-    $.get('/alloff')
+    $.get('/alloff',function(){
+        document.getElementById('led1t').style.backgroundColor="RED"
+        document.getElementById('led2t').style.backgroundColor="RED"
+        document.getElementById('led3t').style.backgroundColor="RED"
+        document.getElementById('led4t').style.backgroundColor="RED"
+        document.getElementById('led5t').style.backgroundColor="RED"
+    })
     document.getElementById('change').onclick = function(){
         $.get('/change',function(){
             location.reload();
@@ -8,67 +14,77 @@ $(window).on('load', function () {
     }
     document.getElementById('led1').onclick = function(){
         var state = document.getElementById('led1t').innerHTML.toString()
-        if(state=="Currently LED1 is OFF"){
+        if(state=="OFF"){
             $.get('/led1/on',function(data){
                 console.log("here is "+data)
-                document.getElementById('led1t').innerHTML="Currently LED1 is ON"
+                document.getElementById('led1t').innerHTML="ON"
+                document.getElementById('led1t').style.backgroundColor="GREEN"
             })
         }
         else{
             $.get('/led1/off',function(){
-                document.getElementById('led1t').innerHTML="Currently LED1 is OFF"
+                document.getElementById('led1t').innerHTML="OFF"
+                document.getElementById('led1t').style.backgroundColor="RED"
             })
         }
     }
     document.getElementById('led2').onclick = function(){
         var state = document.getElementById('led2t').innerHTML.toString()
-        if(state=="Currently LED2 is OFF"){
+        if(state=="OFF"){
             $.get('/led2/on',function(){
-                document.getElementById('led2t').innerHTML="Currently LED2 is ON"
+                document.getElementById('led2t').innerHTML="ON"
+                document.getElementById('led2t').style.backgroundColor="GREEN"
             })
         }
         else{
             $.get('/led2/off',function(){
-                document.getElementById('led2t').innerHTML="Currently LED2 is OFF"
+                document.getElementById('led2t').innerHTML="OFF"
+                document.getElementById('led2t').style.backgroundColor="RED"
             })
         }
     }
     document.getElementById('led3').onclick = function(){
         var state = document.getElementById('led3t').innerHTML.toString()
-        if(state=="Currently LED3 is OFF"){
+        if(state=="OFF"){
             $.get('/led3/on',function(){
-                document.getElementById('led3t').innerHTML="Currently LED3 is ON"
+                document.getElementById('led3t').innerHTML="ON"
+                document.getElementById('led3t').style.backgroundColor="GREEN"
             })
         }
         else{
             $.get('/led3/off',function(){
-                document.getElementById('led3t').innerHTML="Currently LED3 is OFF"
+                document.getElementById('led3t').innerHTML="OFF"
+                document.getElementById('led3t').style.backgroundColor="RED"
             })
         }
     }
     document.getElementById('led4').onclick = function(){
         var state = document.getElementById('led4t').innerHTML.toString()
-        if(state=="Currently LED4 is OFF"){
+        if(state=="OFF"){
             $.get('/led4/on',function(){
-                document.getElementById('led4t').innerHTML="Currently LED4 is ON"
+                document.getElementById('led4t').innerHTML="ON"
+                document.getElementById('led4t').style.backgroundColor="GREEN"
             })
         }
         else{
             $.get('/led4/off',function(){
-                document.getElementById('led4t').innerHTML="Currently LED4 is OFF"
+                document.getElementById('led4t').innerHTML="OFF"
+                document.getElementById('led4t').style.backgroundColor="RED"
             })
         }
     }
     document.getElementById('led5').onclick = function(){
         var state = document.getElementById('led5t').innerHTML.toString()
-        if(state=="Currently LED5 is OFF"){
+        if(state=="OFF"){
             $.get('/led5/on',function(){
-                document.getElementById('led5t').innerHTML="Currently LED5 is ON"
+                document.getElementById('led5t').innerHTML="ON"
+                document.getElementById('led5t').style.backgroundColor="GREEN"
             })
         }
         else{
             $.get('/led5/off',function(){
-                document.getElementById('led5t').innerHTML="Currently LED5 is OFF"
+                document.getElementById('led5t').innerHTML="OFF"
+                document.getElementById('led5t').style.backgroundColor="RED"
             })
         }
     }
